2df7895e1ad4823db045cbd2fe7ae2ab799accdc
Ignacio C <icorrao90@gmail.com>
2023-05-25 17:04:44 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+const puerto = 3000;
+
+app.use(express.static('public'));
+
+ app.get("/", (req, res) => {
+     res.render('pages/index')
+ });
+
+app.set('view engine', 'ejs')
+
+// redireccionar a otras secciones de la pagina
+// app.get('/articles',(req,res){
+//   res.render('pages/seccion-a-redireccionar')
+// })
+
+app.listen(puerto, () => {
+  console.log("servidor ejecutado");
+});
