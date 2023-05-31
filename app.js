562806590840bb50d1052b3136dfa36b3851bdb9
Lcdiego <diegocolman78@hotmail.com>
2023-05-30 22:48:42 -0300

    reorganice las carpetas la carpeta partial,agregue carpeta router y rutie algunas paguinas del footer

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
+import { name } from "ejs";
 import express from "express";
+import nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+import formRouter from './routes/formulario.js'
+
 
 
 const app = express();
 const puerto = 3000;
+dotenv.config({path: './.env'})
 
-
+app.use(formRouter)
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
-
- app.get("/", (req, res) => {
-     res.render('pages/index')
- });
-
 app.set('view engine', 'ejs')
 
 // redireccionar a otras secciones de la pagina
 // app.get('/articles',(req,res){
 //   res.render('pages/seccion-a-redireccionar')
 // })
+
+
+
+
+
+
+
+
+
+
 
 app.listen(puerto, () => {
   console.log("servidor ejecutado");
---
+++ routes/formulario.js
@@ -0,0 +1,68 @@
+import express from 'express'
+const router=express.Router()
+
+// Get del footer
+router.get("/politicas",(req,res)=>{
+   res.render('pages/footer/politicas.ejs')
+
+
+})
+//Get del formilario
+router.get('/enviar-correo',(req,res)=>{
+  res.render('pages/correo/correo.ejs')
+})
+//Get pagina de inicio
+router.get("/", (req, res) => {
+    res.render('pages/index')
+  });
+  
+  
+
+
+
+//Post formulario
+router.post('/enviar-correo', async(req, res) => {
+    const formData = req.body; // Obtiene los datos del formulario
+    console.log(formData.email)
+  
+    // Transporte de Nodemailer, envía el correo electrónico
+  
+    const transporter = nodemailer.createTransport({
+      
+      // Configuración del servidor SMTP
+      host:'smtp.gmail.com',  
+      port : 587,
+      user: '',
+      pass:''
+  });
+  // Opciones del correo electrónico
+    const mailOptions = {
+     from:'',
+     
+        to:'diegocolman14@gmail.com',
+        subjet:`nuevo mensaje de contacto ${formData.nombre}`,
+        text:`
+        Nombre ${formData.nombre}
+        Correo ${formData.email}
+        Mensaje ${formData.mensaje}`
+     };
+  
+  
+    await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        res.send('Error al enviar el correo electrónico');
+      } else {
+        console.log('Correo electrónico enviado: ' + info.response);
+        res.send('Correo electrónico enviado correctamente');
+        
+      }
+      
+    });
+  });
+
+
+
+
+
+  export default router
