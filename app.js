2b126e5c59d6143a36f700c001917b0c21550d12
Ignacio C <icorrao90@gmail.com>
2023-06-15 21:01:32 -0300

    conexion inicial a la api de google libros

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ import express from "express";
 import nodemailer from "nodemailer";
 import dotenv from 'dotenv';
 import formRouter from './routes/formulario.js'
+//api
+import { google } from 'googleapis';
+import { auth } from 'google-auth-library';
+import credentials from './credentials/extreme-surge-389602-d07d7c6c258c.json' assert { type: 'json' };
+const authClient = await auth.getClient({
+  credentials,
+  scopes: ['https://www.googleapis.com/auth/books'],
+});
 
-
+const client = google.books({ version: 'v1', auth: authClient });
+//
 
 const app = express();
 const puerto = 3000;
@@ -25,6 +34,29 @@ app.set('view engine', 'ejs')
 // app.get('/articles',(req,res){
 //   res.render('pages/seccion-a-redireccionar')
 // })
+
+
+//prueba mostrar libros por consola (no funciona)
+async function buscarLibros() {
+  try {
+    const client = await google.books({ version: 'v1', auth: await auth.getClient({
+      credentials,
+      scopes: ['https://www.googleapis.com/auth/books'],
+    })});
+
+    const response = await client.volumes.list({
+      q: 'x', // reemplazar x por nombre de libro
+    });
+
+    console.log(response.data.items);
+  } catch (error) {
+    console.error('error:', error.message);
+  }
+}
+
+buscarLibros();
+
+
 
 
 app.listen(puerto, () => {
