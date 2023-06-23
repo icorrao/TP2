d18870fe7d8fbc2b20b0f701d81fd18457becf41
Ignacio C <icorrao90@gmail.com>
2023-06-22 22:05:55 -0300

    conexion a mongo atlas y pruebas con la api

--- app.js
+++ app.js
@@ -2,11 +2,29 @@ import ejs from "ejs";
 import express from "express";
 import nodemailer from "nodemailer";
 import dotenv from 'dotenv';
+dotenv.config();
 import formRouter from './routes/formulario.js'
+//axios
+import axios from 'axios'
+//path
+import  path  from "path";
 //api
+const API_KEY = process.env.API_KEY;
 import { google } from 'googleapis';
-const client = google.books({ version: 'v1', auth: 'x' }); // reemplazar x por tu api key
-//
+//mongoose
+import mongoose from "mongoose";
+const mongoUser = process.env.MONGO_USER;
+const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoCluster = process.env.MONGO_CLUSTER;
+const mongoDatabase = process.env.MONGO_DATABASE;
+mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPassword}@${mongoCluster}/${mongoDatabase}?retryWrites=true&w=majority`)
+.then(() => {
+  console.log("conectado a mongodb")
+}).catch((error) => {
+  console.log(error)
+})
+//body parser
+import bodyParser from "body-parser";
 
 const app = express();
 const puerto = 3000;
@@ -14,29 +32,47 @@ dotenv.config({path: './.env'})
 
 app.use(formRouter)
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended:true})) //body parser
 app.use(express.static('public'));
 app.use('/node_modules', express.static('node_modules')); // Carpeta node_modules
-
  app.get("/", (req, res) => {
      res.render('pages/index')
  });
 
 app.set('view engine', 'ejs')
 
-// redireccionar a otras secciones de la pagina
-// app.get('/articles',(req,res){
-//   res.render('pages/seccion-a-redireccionar')
-// })
+//pruebas
 
+app.get('/comprar', async (req, res) => {
+  const query = req.query.buscar || '';
 
-//prueba mostrar libros por consola
-const response = await client.volumes.list({
-  q: 'x', // reemplazar x por nombre de libro
+  try {
+    const books = await buscarPorConsulta(query);
+    res.render('pages/partials/comprar', { books });
+  } catch (error) {
+    console.error('Error en la búsqueda de libros:', error.message);
+    res.render('pages/comprar', { books: [] });
+  }
 });
 
-console.log(response.data.items);
 
-
+async function buscarPorConsulta(query) {
+  try {
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}&langRestrict=es&key=${API_KEY}`;
+    const response = await axios.get(url);
+    return response.data.items
+      .filter((book) => book.volumeInfo && book.volumeInfo.language === 'es' && book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail && book.volumeInfo.authors) // Filtrar libros con descripciones en castellano, con imagen y con autor
+      .map((book) => ({
+        title: book.volumeInfo.title,
+        authors: book.volumeInfo.authors,
+        publisher: book.volumeInfo.publisher,
+        thumbnail: book.volumeInfo.imageLinks.thumbnail, // Utilizar la propiedad thumbnail de la API de Google Books
+      }));
+  } catch (error) {
+    throw new Error('Error en la búsqueda de libros');
+  }
+}
+//
 
 
 app.listen(puerto, () => {
